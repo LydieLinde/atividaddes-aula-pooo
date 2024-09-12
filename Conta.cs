79fcaf71780bbc50b5d9fbb071b785ddbbfd5561
LidiaKarla <145567054+LydieLinde@users.noreply.github.com>
2024-09-12 16:35:14 -0300

    first commit

---
+++ Conta.cs
@@ -0,0 +1,16 @@
+public class Contabancaria{
+
+    //atributos 
+    public string email;
+    public string nome;
+    public int contato;
+
+    //Construtorda classe(para inicializar os atributos)
+    public Contabancaria(string emailDaConta, string nomeDaConta, int contatoDaConta);
+    {
+        email = emailDaConta;
+        nome = nomeDaConta;
+        contato = contatoDaConta;
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+class Principal{
+    static void Main(string[] args){
+
+        //criando um objeto da classe conta bancaria 
+        Conta minhaPrimeira = new Carro("lidia.k@hotemail.com", "lidia", "849");
+    }
+}
